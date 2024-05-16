f8e381dc278b61ed500a04ab57cc9751e3fc3ce3
sapnajohn01 <167646220+sapnajohn01@users.noreply.github.com>
2024-05-16 15:44:22 +0530

    Day1.py

--- Day1.py
+++ Day1.py
@@ -14,8 +14,6 @@ These lives remain precious to our country and infinitely precious to many of yo
 There was horror at the scale of destruction. and awe at the bravery and kindness that rose to meet it. There was, shock! at the audacity of evil and gratitude, for the heroism and decency that opposed it? In the sacrifice!
 """
 
-txt1="Positive thinking is all. A matter of habits If you are; not quite a positive thinker Change Yourself?"
-
 s1="20 years ago, we all found in different ways in different places but all at the same moment."
 s2="That our lives would be changed forever. The world was loud with carnage and sirens and then quiet with missing voices that would never be heard again."
 s3="These lives remain precious to our country and infinitely precious to many of you. Today, we remember your loss, we share your sorrow, and we honor the men and women you have loved so long and so well. For those too young to recall that clear September day, it is hard to describe the mix of feelings we experienced. "
@@ -23,15 +21,12 @@ s4="There was horror at the scale of destruction. and awe at the bravery and kin
 
 #txt=s1+s2+s3+s4
 
-txt=s2
+txt=s2 + s2
 
 sep_words= word_tokenize(txt)
 sep_sent=sent_tokenize(txt)
 # type(sep_words)
 print(sep_sent)
-
-new_arr=np.array(sep_words)
-#print(new_arr)
 
 #stop_words=set(stopwords.words('english'))
 stop_words=["is",".",";","?"]
